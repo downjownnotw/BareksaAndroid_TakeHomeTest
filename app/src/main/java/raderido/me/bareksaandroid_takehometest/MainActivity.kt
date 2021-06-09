118e65f07a4336b764b0315a5dfd8ad795598ae2
Rade Rido Pangaribuan <rade.rido@logkar.com>
2021-06-09 15:25:04 +0700

    Initial commit

---
+++ app/src/main/java/raderido/me/bareksaandroid_takehometest/MainActivity.kt
@@ -0,0 +1,11 @@
+package raderido.me.bareksaandroid_takehometest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
